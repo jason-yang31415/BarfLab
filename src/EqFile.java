319418d1f84e7fdad4724a89cf89fb8eefc194f1
jason-yang31415 <jason.yang31415@gmail.com>
2015-04-08 14:27:01 -0400

    Added some Eq Parsing

--- src/EqFile.java
+++ src/EqFile.java
@@ -2,10 +2,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class EqFile {
 
+	String[] numeralsArray = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+	ArrayList<String> numerals = new ArrayList<String>(Arrays.asList(numeralsArray));
+	String[] opsArray = {"+", "-", "*", "/", "^"};
+	ArrayList<String> ops = new ArrayList<String>(Arrays.asList(opsArray));
+	
 	public static void main(String[] args){
 		try {
 			new EqFile();
@@ -23,18 +29,20 @@ public class EqFile {
 			if (line.startsWith("EQ ")){
 				String equation = line.split("EQ ")[1];
 				String[] line1 = equation.split(" = ");
-				String left = line1[0];
-				String right = line1[1];
+				String leftString = line1[0];
+				String rightString = line1[1];
 				
-				parseExpression(left);
-				parseExpression(right);
+				//Thing left = parseExpression(leftString);
+				//Thing right = parseExpression(rightString);
+				parseExpression(leftString);
+				parseExpression(rightString);
 			}
 		}
 		reader.close();
 	}
 	
 	public void parseExpression(String e){
-		int rPpos = e.indexOf(")");
+		/*int rPpos = e.indexOf(")");
 		if (rPpos >= 0){
 			String rPstring = e.substring(0, rPpos);
 			int lPpos = rPstring.lastIndexOf("(");
@@ -59,17 +67,85 @@ public class EqFile {
 		}
 		else {
 			System.out.println(e);
-		}
+		}*/
+		
+		Thing thing;
 		
 		e = e.replaceAll("\\s", "");
+		String[] stringArray = e.split("");
+		ArrayList<String> string = new ArrayList<String>(Arrays.asList(stringArray));
+		int parentheses = 0;
+		int opOrder = -1;
+		int opPos = -1;
+		
+		for (String s : string){
+			if (s.equals("("))
+				parentheses++;
+			
+			if (s.equals(")"))
+				parentheses--;
+			
+			//If character is outside parentheses
+			if (parentheses == 0){
+				if (!s.equals("(") && !s.equals(")")){
+					//If character is an op
+					if (ops.contains(s)){
+						if (opOrder >= 0 && opPos >= 0){
+							if (opOrder > ops.indexOf(s)){
+								opOrder = ops.indexOf(s);
+								opPos = string.indexOf(s);
+							}
+						}
+						else {
+							opOrder = ops.indexOf(s);
+							opPos = string.indexOf(s);
+						}
+					}
+				}
+			}
+		}
+		
+
+		//Check if string e contains any ops outside parentheses
+		if (opOrder >= 0 && opPos >= 0){
+			String leftString = e.substring(0, opPos);
+			String rightString = e.substring(opPos + 1, e.length());
+			System.out.println(leftString);
+			System.out.println(rightString);
+		}
+		//Does not contain any ops outside parentheses
+		else {
+			boolean containsOp = false;
+			for (String s : string){
+				//Check if contains ops inside parentheses
+				if (ops.contains(s)){
+					containsOp = true;
+				}
+			}
+			
+			//Check if contains ops inside parentheses
+			if (containsOp){
+				
+			}
+			else {
+				//Parse e as constant or var
+				if (isUpper(e)){
+					thing = new Value(0, true);
+				}
+				else {
+					thing = new Value(Float.parseFloat(e), false);
+				}
+			}
+		}
+		
+		/*e = e.replaceAll("\\s", "");
 		String[] stringArray = e.split("");
 		for (String s : stringArray){
 			if (isAlpha(s) && isUpper(s)){
 				Thing var = new Value(0f, false);
 				int varPos = e.indexOf(s);
-				
 			}
-		}
+		}*/
 	}
 	
 	public boolean isAlpha(String s){
