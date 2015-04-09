597bcd1242859bb9985f1068c14ef64de3ba193b
jason-yang31415 <jason.yang31415@gmail.com>
2015-04-09 16:51:31 -0400

    Changed JFileChooser to awt FileDialog
    FileDialog has native OS file chooser interface and supports native OS
    shortcuts.

--- src/Window.java
+++ src/Window.java
@@ -1,10 +1,13 @@
+import java.awt.FileDialog;
 import java.awt.FlowLayout;
-import java.awt.event.*;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.IOException;
 
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Window extends JFrame {
 	
@@ -44,7 +47,7 @@ public class Window extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			if (event.getSource() == button){
-				final JFileChooser fc = new JFileChooser();
+				/*final JFileChooser fc = new JFileChooser();
 				FileNameExtensionFilter filter = new FileNameExtensionFilter("EQ Equations", "eq");
 				fc.setFileFilter(filter);
 				int returnVal = fc.showOpenDialog(Window.this);
@@ -55,7 +58,11 @@ public class Window extends JFrame {
 		            doStuff(file.getPath());
 		        } else {
 		            System.out.println("screwed up");
-		        }
+		        }*/
+				FileDialog fc = new java.awt.FileDialog((java.awt.Frame) null);
+				fc.setVisible(true);
+				String path = fc.getDirectory() + fc.getFile();
+				doStuff(path);
 			}
 		}
 		
